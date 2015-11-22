348c79534a7235c4092dfadfe084629fcadd1e76
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-11-22 16:01:45 -0200

    Add create and destroy tenant methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,12 @@
 class Company < ActiveRecord::Base
+  after_create :create_tenant
+  after_destroy :destroy_tenant
+
+  def create_tenant
+    Apartment::Tenant.create(subdomain)
+  end
+
+  def destroy_tenant
+    Apartment::Tenant.drop(subdomain)
+  end
 end
