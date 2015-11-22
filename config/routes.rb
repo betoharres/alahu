7b94584248e85523ab64c5188c1d5e8dc4e3bc8e
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-11-22 13:31:41 -0200

    Generate devise_token_auth code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :companies, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
