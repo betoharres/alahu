24333fafc620fd32343cd1ae969a69402a120e6c
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-12-02 22:47:33 -0200

    Add stub to login in tests
    Also add database cleaner

---
+++ spec/support/request_helper.rb
@@ -0,0 +1,8 @@
+module Request
+  module RequestsHelpers
+    def set_authentication_headers_for(user)
+      user_headers = user.create_new_auth_token
+      @request.headers.merge!(user_headers)
+    end
+  end
+end
