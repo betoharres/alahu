e3e38b1a666ec82daf524e0d8f59b7907a01b77a
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-12-17 15:29:32 -0200

    Role's name field as unique and case insensitive

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :user_role
   has_many :users, through: :user_role
+
+  validates_uniqueness_of :name
 end
--- db/migrate/20151203195547_create_roles.rb
+++ db/migrate/20151203195547_create_roles.rb
@@ -1,8 +1,7 @@
 class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles, id: :uuid, default: 'gen_random_uuid()' do |t|
-      t.string :name
-      t.integer :ability, limit: 2
+      t.citext :name, unique: true
 
       t.timestamps null: false
     end
