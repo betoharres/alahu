1990bc376106710690102afc6c5b1e7492dd5379
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-12-29 15:24:45 -0200

    Create permission to user destroy his record after create record

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ActiveRecord::Base
 
   has_one :user_role
   has_one :role, through: :user_role
+  has_one :permission, as: :resourceable, dependent: :destroy
 
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  after_create :add_permission_to_user
+
+  def add_permission_to_user
+    Permission.create resourceable_id: id, resourceable_type: self.class.to_s,
+                      ability: 15        , role: nil
+  end
 end
