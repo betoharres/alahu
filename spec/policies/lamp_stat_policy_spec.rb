cdfeb40cc55a93b44d5e50e2806f45a88ee173e6
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-04-25 16:26:22 -0300

    Add lamp_stat_policy_spec

---
+++ spec/policies/lamp_stat_policy_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe LampStatPolicy, type: :policy do
+
+  create_not_authorized_user
+
+  before :all do
+    Apartment::Tenant.switch!(@company.subdomain)
+    @record = FactoryGirl.create(:lamp_stat)
+  end
+
+  subject { described_class }
+
+  permissions :show? do
+    it 'permits showing LampStat to user' do
+      expect(subject).to permit(@user, @record)
+    end
+  end
+
+  permissions :create? do
+    it 'denies creating LampStat if user does not have authorization' do
+      expect(subject).not_to permit(@user, @record)
+    end
+  end
+
+  permissions :update? do
+    it 'denies creating LampStat if user does not have authorization' do
+      expect(subject).not_to permit(@user, @record)
+    end
+  end
+
+  permissions :destroy? do
+    it 'denies creating LampStat if user does not have authorization' do
+      expect(subject).not_to permit(@user, @record)
+    end
+  end
+end
