3ae5a6b3eefcd3ba8f6be657c698ec89be090831
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-05-02 17:34:28 -0300

    Node belongs to Network

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,4 @@
 class Network < ActiveRecord::Base
+  has_many :nodes
   validates_presence_of :name
 end
--- db/migrate/20160502133953_create_nodes.rb
+++ db/migrate/20160502133953_create_nodes.rb
@@ -3,6 +3,7 @@ class CreateNodes < ActiveRecord::Migration
     create_table :nodes, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.citext :name, unique: true, null: false
       t.st_point :location, geographic: true, has_z: true
+      t.references :network, index: true, foreign_key: true, type: :uuid
 
       t.timestamps null: false
     end
