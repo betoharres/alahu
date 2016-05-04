c4f943dcfa2acbf1efcb446a0eb40b43b8a9896b
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-05-04 17:17:50 -0300

    Add validations to Firmware model

--- app/models/firmware.rb
+++ app/models/firmware.rb
@@ -1,4 +1,5 @@
 class Firmware < ActiveRecord::Base
   has_many :permissions, as: :resourceable, dependent: :destroy
-  validates_presence_of :path
+  validates_presence_of   :path, :name
+  validates_uniqueness_of :path, :name
 end
